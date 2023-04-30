fb4252d48207751caba4d8d49d8c2207d6101521
MaksymusPrime <grafdemaxo@gmail.com>
2023-05-01 00:14:40 +0300

    scroll

--- js/app.js
+++ js/app.js
@@ -20,3 +20,18 @@ myVid.volume=0.5;
 
 
 
+
+let sc = document.getElementById('sc');
+
+document.addEventListener('scroll', function() {
+    // Get scroll position
+    let scrollPosition = window.pageYOffset;
+
+    // Calculate whether the scroll it 350px or not then set the opacity to face the header out
+    if (scrollPosition <= 0) {
+        sc.style.opacity = 1 - scrollPosition / 2;
+    } else {
+        sc.style.opacity = 0;
+    }
+
+});
